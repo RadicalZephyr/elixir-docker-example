74e6b7aba3dd7d865a8974d5cc3815d82f98f881
Geoff Shannon <geoffpshannon@gmail.com>
2017-09-29 15:07:23 -0500

    Add users

---
+++ lib/debian_web/controllers/fallback_controller.ex
@@ -0,0 +1,20 @@
+defmodule DebianWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use DebianWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(DebianWeb.ChangesetView, "error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(DebianWeb.ErrorView, :"404")
+  end
+end
